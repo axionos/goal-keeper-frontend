0b70a380584cc2e962dedfc98154321ffc5a6abb
axionos <axionoso@gmail.com>
2019-10-10 16:43:32 -0400

    install bootstrap and build goal add form

--- src/containers/GoalList.js
+++ src/containers/GoalList.js
@@ -1,34 +1,65 @@
 import React from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 class GoalList extends React.Component {
   state = {
-    user: ''
+    user: "",
+    startDate: new Date(),
+    endDate: new Date()
   }
 
   componentDidMount(){
     if (!localStorage.getItem('token')){
       this.props.router.history.push('/login')
     }
-    // fetch('http://localhost:3000/profile', {
-    //   headers: {
-    //     'Authorization': localStorage.getItem('token')
-    //   }
-    // })
-    // .then(res => res.json())
-    // .then(data => {
-    //   console.log(data)
-    //   this.setState({
-    //     user: data
-    //   })
-    // })
+  }
+
+  handleClickAdd = (e) => {
+    e.preventDefault()
+    console.log('submit')
+  }
+
+  handleClickShow = () => {
+    console.log('show')
+  }
+
+  // UPDATE START DATE
+  handleChangeStartDate = date => {
+    this.setState({
+      startDate: date
+    })
+  }
+
+  // UPDATE END DATE TO THE STATE
+  handleChangeEndDate = date => {
+    this.setState({
+      endDate: date
+    })
   }
 
   render(){
     console.log('goal list props', this.props);
+    console.log('goal list state', this.state);
     return (
       <div className="goal-list">
         <h1>Hello, {this.props.user.username}</h1>
-        This is the GoalList Page
+        <button onClick={this.handleClickShow}>+</button>
+
+        <div className="form-container">
+          <form onSubmit={this.handleClickAdd}>
+            <input type="text" name="title" placeholder="Goal Title"/>
+            <DatePicker
+              selected={this.state.startDate}
+              onChange={this.handleChangeStartDate} />
+            <DatePicker
+              selected={this.state.endDate}
+              onChange={this.handleChangeEndDate} />
+            <br />
+            <input type="submit" value="Submit"/>
+          </form>
+        </div>
       </div>
     );
   }
